cc9d5be79ea12eb32cf0222cf2866eef734f5c77
Junerver <junerver@qq.com>
2022-08-25 08:30:43 +0800

    体验Modifier

--- app/src/main/java/com/example/composewithredux/MainActivity.kt
+++ app/src/main/java/com/example/composewithredux/MainActivity.kt
@@ -3,14 +3,17 @@ package com.example.composewithredux
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.composewithredux.redux.*
 import com.example.composewithredux.ui.theme.ComposeWithReduxTheme
 import xyz.junerver.compose_redux.StoreProvider
@@ -45,7 +48,21 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun ShowName() {
     val name by selectState<AppState, String?> { name }
-    Text(text = "show redux: $name!")
+    MyText(text = name ?: "")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun MyText(text:String = "xzxcasasdasdasddasdasdzxc"){
+    Text(
+        text = "show redux: $text!",
+        modifier = Modifier
+            .size(150.dp)
+            .background(Color.Red),
+        textAlign = TextAlign.Center,
+        lineHeight = 30.sp,
+        fontSize = 15.sp
+    )
 }
 
 @Composable
